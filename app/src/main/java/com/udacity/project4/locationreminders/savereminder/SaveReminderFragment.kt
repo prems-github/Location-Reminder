5a97dceed1bae77632e9847f517ef7ee04ac39c7
Prem <premdandy@gmail.com>
2021-07-04 00:16:28 +0530

    request background permission with explanatory dialogue and snackbar

--- app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
+++ app/src/main/java/com/udacity/project4/locationreminders/savereminder/SaveReminderFragment.kt
@@ -1,11 +1,22 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import android.Manifest
+import android.content.DialogInterface
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
+import com.google.android.material.snackbar.Snackbar
+import com.udacity.project4.BuildConfig
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.base.NavigationCommand
@@ -17,6 +28,9 @@ class SaveReminderFragment : BaseFragment() {
     //Get the view model this time as a single to be shared with the another fragment
     override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSaveReminderBinding
+    private val runningQOrLater =
+        android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q
+    private val BACKGROUND_LOCATION_REQUEST=2
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -45,33 +59,99 @@ class SaveReminderFragment : BaseFragment() {
                 NavigationCommand.To(SaveReminderFragmentDirections.actionSaveReminderFragmentToSelectLocationFragment())
         }
 
+
+
         binding.saveReminder.setOnClickListener {
-            checkEmptyOrNot()
-            val title = _viewModel.reminderTitle.value
-            val description = _viewModel.reminderDescription.value
-            val location = _viewModel.reminderSelectedLocationStr.value
-            val latitude = _viewModel.latitude.value
-            val longitude = _viewModel.longitude.value
+            if(!isFieldsAreEmpty()){
+                if (runningQOrLater) {
+                    if (isBackgroundPermissionEnabled()) {
+                        Toast.makeText(requireActivity(),"Background Location approved",Toast.LENGTH_SHORT).show()
+                    }else{
+                        AlertDialog.Builder(requireActivity())
+                            .setTitle(R.string.all_time_permission_title)
+                            .setMessage(R.string.background_permission_explanation)
+                            .setPositiveButton("OK", DialogInterface.OnClickListener { dialogInterface, i ->
+                                requestPermissions(
+                                    arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+                                    BACKGROUND_LOCATION_REQUEST
+                                )
+                            })
+                            .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialogInterface, i ->
+                                dialogInterface.dismiss()
+                            }).create().show()
+                        /*requestPermissions(
+                            arrayOf(Manifest.permission.ACCESS_BACKGROUND_LOCATION),
+                            BACKGROUND_LOCATION_REQUEST
+                        )*/
+                    }
+                }else{
+                    saveReminderInDB()
+                    addGeoFenceRequest()
 
-   //         Toast.makeText(requireActivity(),"$title,$description,$location,$latitude,$longitude",Toast.LENGTH_SHORT).show()
+                }
+            }
 
-//            TODO: use the user entered reminder details to:
-//             1) add a geofencing request
-//             2) save the reminder to the local db
+
         }
     }
 
+    private fun saveReminderInDB() {
+        TODO("Not yet implemented")
+    }
+
+    private fun addGeoFenceRequest() {
+        TODO("Not yet implemented")
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+       if(requestCode==BACKGROUND_LOCATION_REQUEST){
+           if(grantResults.isEmpty() || grantResults[0]==PackageManager.PERMISSION_DENIED){
+              Snackbar.make(
+                   binding.root,
+                   R.string.background_permission_denied_explanation,
+                   Snackbar.LENGTH_INDEFINITE
+               ).also {
+                   val snackbarTextView=(it.view.findViewById(R.id.snackbar_text)) as TextView
+                  snackbarTextView.maxLines=3
+              }
+                  .setAction(R.string.settings) {
+                       startActivity(Intent().apply {
+                           action = Settings.ACTION_APPLICATION_DETAILS_SETTINGS
+                           data = Uri.fromParts("package", BuildConfig.APPLICATION_ID, null)
+                           flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                       })
+                   }.show()
+
+           }else{
+               Toast.makeText(requireActivity(),"Background Location approved",Toast.LENGTH_SHORT).show()
+           }
+       }
+    }
+
+    private fun isBackgroundPermissionEnabled() = ContextCompat.checkSelfPermission(
+        requireContext(), android.Manifest.permission.ACCESS_BACKGROUND_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED
+
     //checks the user input fields empty or null and if it so displays toast to fill the same
 
-    private fun checkEmptyOrNot() {
+    private fun isFieldsAreEmpty(): Boolean {
+        var isEmpty=true
         if (binding.reminderTitle.text.isNullOrEmpty() || binding.reminderDescription.text.isNullOrEmpty()
         ) {
             Toast.makeText(requireActivity(), R.string.enter_title_description, Toast.LENGTH_SHORT)
                 .show()
-        }else if(binding.selectedLocation.text.isNullOrEmpty()){
+        } else if (binding.selectedLocation.text.isNullOrEmpty()) {
             Toast.makeText(requireActivity(), R.string.select_location, Toast.LENGTH_SHORT)
                 .show()
+
+        }else{
+            isEmpty=false
         }
+        return isEmpty
     }
 
     override fun onDestroy() {
