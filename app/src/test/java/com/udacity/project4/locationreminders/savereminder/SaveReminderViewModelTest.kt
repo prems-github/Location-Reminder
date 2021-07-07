4b791c37d9f7a6a2b9ab9c9f902bb91453945362
Prem <premdandy@gmail.com>
2021-07-07 20:04:09 +0530

    save reminder VM unit test

--- app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/savereminder/SaveReminderViewModelTest.kt
@@ -1,9 +1,16 @@
 package com.udacity.project4.locationreminders.savereminder
 
+import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.reminderslist.ReminderDataItem
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.core.Is.`is`
+import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.stopKoin
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
@@ -11,6 +18,52 @@ class SaveReminderViewModelTest {
 
 
     //TODO: provide testing to the SaveReminderView and its live data objects
+    private lateinit var saveReminderViewModel: SaveReminderViewModel
+    private lateinit var fakeDataSource: FakeDataSource
+
+    @Before
+    fun setupViewModel() {
+        stopKoin()
+        fakeDataSource = FakeDataSource()
+        saveReminderViewModel = SaveReminderViewModel(
+            ApplicationProvider.getApplicationContext(),
+            fakeDataSource
+        )
+    }
+
+    //Validate the entered data, returns true if its valid else return false in case of null or empty
+    @Test
+    fun validateEnteredData_withValidData_returnstrue() {
+
+        val reminder = ReminderDataItem(
+            title = "New Market",
+            description = "Buy Groceries",
+            location = "Skull Mountain",
+            latitude = -34.0,
+            longitude = 151.0
+        )
+
+        val result=saveReminderViewModel.validateEnteredData(reminder)
+        assertThat(result,`is`(true))
+
+    }
+
+    //Validate the entered data, returns true if its valid else return false in case of null or empty
+    @Test
+    fun validateEnteredData_withInvalidData_returnsFalse() {
+
+        val reminder = ReminderDataItem(
+            title = "",
+            description = "Buy Groceries",
+            location = "Skull Mountain",
+            latitude = -34.0,
+            longitude = 151.0
+        )
+
+        val result=saveReminderViewModel.validateEnteredData(reminder)
+        assertThat(result,`is`(false))
+
+    }
 
 
 }
